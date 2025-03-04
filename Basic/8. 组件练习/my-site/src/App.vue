02b5b97a0792bce198848ee43b61be1015fad26f
JiaSeng02 <jiaseng02@gmail.com>
2025-03-05 02:19:02 +0800

    Init commit no.8

---
+++ Basic/8. 组件练习/my-site/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app-container">
+    <h1>App组件</h1>
+    
+    <siteAside />
+    <empty />
+    <imageLoader />
+    <pager />
+    
+    
+  </div>
+</template>
+
+<script>
+import siteAside from "./components/SiteAside/test.vue";
+import pager from "./components/Pager/test.vue";
+import empty from "./components/Empty/test.vue";
+import imageLoader from "./components/ImageLoader/test.vue"
+
+export default {
+  components: {
+    siteAside,
+    pager,
+    empty,
+    imageLoader
+  },
+};
+</script>
+
+<style>
+.app-container {
+  overflow-y: scroll;
+  height: 100vh;
+}
+</style>
+
+
