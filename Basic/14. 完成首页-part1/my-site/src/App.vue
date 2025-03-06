8f40a81f45e26473a47b49f67e7229012147ca66
JiaSeng02 <jiaseng02@gmail.com>
2025-03-06 16:11:52 +0800

    Init commit no.14

---
+++ Basic/14. 完成首页-part1/my-site/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app-container">
+    <Layout>
+      <template #left>
+        <div class="aside">
+          <SiteAside />
+        </div>
+      </template>
+      <template #default>
+        <RouterView />
+        <img
+          src="http://mdrs.yuanjin.tech/img/20201206153157.png"
+          alt="image-20201206153157145"
+          style="zoom: 40%"
+        />
+      </template>
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Layout from "./components/Layout";
+import SiteAside from "./components/SiteAside";
+export default {
+  components: {
+    Layout,
+    SiteAside,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import "~@/styles/mixin.less";
+.app-container {
+  .self-fill(fixed);
+}
+.aside {
+  width: 250px;
+  height: 100%;
+}
+</style>
