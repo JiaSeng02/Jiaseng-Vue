514cd66646658c81dfa1d3066e7866ab6ec23b8d
JiaSeng02 <jiaseng02@gmail.com>
2025-03-07 15:37:59 +0800

    init commit no.18

---
+++ Basic/18. 组件递归/my-site/src/components/CarouselItem/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="img-item" :class="{ active: isActive }">
+    <img :src="image.img" alt="carousel image" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: Object, // Receive a single image object
+    isActive: Boolean, // Determine if this image is active
+  },
+};
+</script>
+
+<style>
+.img-item {
+  transition: opacity 0.5s ease-in-out;
+  display: none;
+  align-items: center;
+  overflow-y: auto;
+  position: absolute;
+  flex-direction: column;
+  justify-content: center;
+}
+.img-item img {
+  max-width: 50%;
+  height: auto;
+}
+.img-item.active {
+  display: flex; /* Show only the active image */
+}
+</style>
